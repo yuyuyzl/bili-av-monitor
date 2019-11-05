0d1c558100b0665c8276d11cef0d3c18e4aba1c1
yuyuyzl <yuzhongliang22@163.COM>
2019-11-06 01:20:36 +0800

    fix httpapi

--- httpapi.js
+++ httpapi.js
@@ -5,21 +5,45 @@ const FileSync = require('lowdb/adapters/FileSync');
 const http=require("http");
 const adapter = new FileSync('db.json');
 const db = low(adapter);
+const url=require("url");
 
 // Set some defaults (required if your JSON file is empty)
 db.defaults({}).write();
 
-http.createServer((req, res) => {
+let lastRead=Date.now();
 
-    const av=req.url.split("/").pop();
-    const data=db.get(av).value();
-    if(data) {
-        res.writeHead(200, {
-            "content-type": "text/plain; charset=utf-8",
-            "Access-Control-Allow-Origin": "*"
-        });
-        res.end(JSON.stringify(data));
-    }else {
+http.createServer((req, res) => {
+    const urlobj=url.parse(req.url);
+    try {
+        const args = {};
+        urlobj.query.split("&").forEach(s => args[s.split('=')[0]] = s.split("=")[1]);
+        const av = urlobj.pathname.split("/").pop();
+        if(Date.now()-lastRead>10000){
+            db.read();
+            lastRead=Date.now();
+        }
+        const data = db.get(av).value();
+
+        if (data) {
+            res.writeHead(200, {
+                "content-type": "text/plain; charset=utf-8",
+                "Access-Control-Allow-Origin": "*"
+            });
+            if (args.item) {
+                args.item = args.item.split(",");
+            }
+            if (args.item)
+                res.end(JSON.stringify(data.map(obj => args.item.map(key => obj[key]))));
+            else
+                res.end(JSON.stringify(data));
+        } else {
+            res.writeHead(404, {
+                "content-type": "text/plain; charset=utf-8",
+                "Access-Control-Allow-Origin": "*"
+            });
+            res.end("404 Not found");
+        }
+    }catch (e) {
         res.writeHead(404, {
             "content-type": "text/plain; charset=utf-8",
             "Access-Control-Allow-Origin": "*"
