ac735fd72c43d7c60245a6e82e77984132bd4934
yuyuyzl <yuzhongliang22@163.COM>
2019-11-10 17:51:35 +0800

    使用url中after参数进行updatedAt过滤，支持前端增量更新

--- httpapi.js
+++ httpapi.js
@@ -6,6 +6,7 @@ const Sequelize=require('sequelize');
 const dbmodel=require('./dbmodel');
 
 const sql=new Sequelize(...conf.sequelize);
+const Op=Sequelize.Op;
 const models=dbmodel(sql);
 const Monitoring=models.Monitoring;
 const Data=models.Data;
@@ -14,7 +15,7 @@ http.createServer(async (req, res) => {
     const urlobj=url.parse(req.url);
     try {
         const args = {};
-        if(urlobj.query)urlobj.query.split("&").forEach(s => args[s.split('=')[0]] = s.split("=")[1]);
+        if(urlobj.query)urlobj.query.split("&").forEach(s => args[s.split('=')[0]] = unescape(s.split("=")[1]));
         const av = urlobj.pathname.split("/").pop();
         if (av==="action"){
             if(conf.api.secret.indexOf(args.secret)>=0){
@@ -25,7 +26,7 @@ http.createServer(async (req, res) => {
                             interval:args.interval?args.interval:conf.api.defaultIntervalAV,
                             expireDate:args.expireDate?new Date(args.expireDate):null,
                             title:args.title
-                        }],{updateOnDuplicate:["av","interval","expireDate","title"]}).then(()=>{});
+                        }],{updateOnDuplicate:["av","interval","expireDate","title"]});
                         res.writeHead(200, {
                             "content-type": "text/plain; charset=utf-8",
                             "Access-Control-Allow-Origin": "*"
@@ -39,7 +40,7 @@ http.createServer(async (req, res) => {
                             interval:args.interval?args.interval:conf.api.defaultIntervalUser,
                             monitorWithinDays:args.monitorWithinDays,
                             defaultInterval:args.defaultInterval
-                        }],{updateOnDuplicate:["mid","monitorWithinDays","defaultInterval","interval"]}).then(()=>{});
+                        }],{updateOnDuplicate:["mid","monitorWithinDays","defaultInterval","interval"]});
                         res.writeHead(200, {
                             "content-type": "text/plain; charset=utf-8",
                             "Access-Control-Allow-Origin": "*"
@@ -52,9 +53,20 @@ http.createServer(async (req, res) => {
         }
         let data;
         if (av === "monitor") {
-            data = await Monitoring.findAll();
+            data = await Monitoring.findAll({where:{
+                updatedAt:{
+                    [Op.gte]:args.after?new Date(args.after):new Date(0)
+                }
+            }},{order:[["updatedAt","desc"]]});
         } else {
-            data = await Data.findAll({where: {aid: +av}, order: ["time"]});
+            data = await Data.findAll({
+                where: {
+                    aid: +av,
+                    updatedAt: {
+                        [Op.gte]: args.after ? new Date(args.after) : new Date(0)
+                    }
+                }, order: ["time"]
+            });
         }
         if (data) {
             res.writeHead(200, {
@@ -65,12 +77,20 @@ http.createServer(async (req, res) => {
                 args.item = args.item.split(",");
             }
             if (args.item)
-                res.end(JSON.stringify(data.map(obj => args.item.map(key => obj[key]))));
+                res.end(JSON.stringify({
+                    data:data.map(obj => args.item.map(key => obj[key])),
+                    updatedAt:new Date().toISOString()
+                }));
             else
-                res.end(JSON.stringify(data));
+                res.end(JSON.stringify({
+                    data:data,
+                    updatedAt:new Date().toISOString()
+                }));
             return ;
         }
-    }catch (e) {}
+    }catch (e) {
+        console.error(e)
+    }
     res.writeHead(404, {
         "content-type": "text/plain; charset=utf-8",
         "Access-Control-Allow-Origin": "*"
